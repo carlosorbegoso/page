e2da0e692184b90513fa5c905f3d905cf8a5b05c
Carlos <car_11.09@hotmail.com>
2025-08-29 01:01:40 -0500

    feature: add new congi

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '.',
   publicDir: 'assets',
+  base: './', // Esto asegura que las rutas sean relativas
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
